b801f9eeb6733116da3f3d1d50e5930a1bd01798
Sunstar123 <54781339+Sunstar123@users.noreply.github.com>
2020-08-19 08:14:09 -0400

    Add files via upload

---
+++ scripts/Draggable.cs
@@ -0,0 +1,49 @@
+﻿// recomended to not use with other movement scripts, or to disable those scripts while the player drags an object.
+// ask me to make any script changes if needed
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draggable : MonoBehaviour
+{
+    private float startPosX;
+    private float startPosY;
+    private bool isheld = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isheld == true)
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY, 0);
+        }
+    }
+
+    // Called when mouse presses this object
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            startPosX = mousePos.x - this.transform.localPosition.x;
+            startPosY = mousePos.y - this.transform.localPosition.y;
+
+            isheld = true;
+
+        }
+    }
+
+    // Called when mouse releases it's press on this object
+    private void OnMouseUp()
+    {
+        isheld = false;
+    }
+}
